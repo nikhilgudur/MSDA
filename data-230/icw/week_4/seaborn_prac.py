7638234a5b6a07cd5cd03601a7bb771e3f8ca2e0
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2023-09-17 19:34:06 -0700

    ALL

---
+++ data-230/icw/week_4/seaborn_prac.py
@@ -0,0 +1,26 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+fig = plt.figure(figsize=(6, 8))
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# themes = ['darkgrid', 'whitegrid', 'dark', 'white', 'ticks']
+
+# for i, theme in enumerate(themes):
+#     with sns.axes_style(theme):
+#         sns.lineplot(x=x, y=y)
+
+
+# plt.tight_layout()
+# plt.show()
+
+categories = ["Math", "English", "History", "Art"]
+values = [85, 92, 78, 88, 95]
+
+plt.figure(figsize=(6, 2))
+sns.barplot(x=categories, y=values)
+plt.show
